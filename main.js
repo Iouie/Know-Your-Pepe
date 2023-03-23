e3a25c369321b5de577c800a801c60121280ba1c
Louie <sxl7953@rit.edu>
2023-03-22 21:27:56 -0400

    added win screen and reset button

--- main.js
+++ main.js
@@ -7,9 +7,12 @@ document.addEventListener("DOMContentLoaded", () => {
   let firstCard, secondCard;
   let hsName = "";
   let matches = 0;
+  const mainContainer = document.querySelector(".container");
   const container = document.querySelector(".gridContainer");
   const musicButton = document.querySelector("#music");
   const playButton = document.querySelector("#play");
+  const winText = document.querySelector("#win");
+  const mmButton = document.querySelector("#main-menu");
   const mainButtons = document.querySelector(".main-buttons");
   const hsButton = document.querySelector("#highscores");
   const getName = document.querySelector("#name");
@@ -52,6 +55,8 @@ document.addEventListener("DOMContentLoaded", () => {
   createBoard();
   container.style.display = "none"; // intialized to display nothing
   getName.style.display = "none";
+  winText.style.display = "none";
+  mmButton.style.display = "none";
   const cards = document.querySelectorAll(".memory-card");
 
   // function to play and pause music
@@ -124,6 +129,13 @@ document.addEventListener("DOMContentLoaded", () => {
     firstCard.removeEventListener("click", flipCard);
     secondCard.removeEventListener("click", flipCard);
     matches++;
+
+    // win condition
+    if (matches === 8) {
+      winText.style.display = "flex";
+      container.style.display = "none";
+      mmButton.style.display = "flex";
+    }
     resetBoard();
   };
 
@@ -148,6 +160,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // select all container if a card is selcted call flipCard else do nothing
   container.addEventListener("click", (e) => {
+    e.preventDefault();
     if (e.target.className === "back-face") {
       flipCard(e.target);
     }
@@ -166,16 +179,32 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // eventlistener for play button
   playButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    getName.value = "";
     mainButtons.style.display = "none";
     getName.style.display = "flex";
   });
 
   // eventlistener to get name if enter is pressed
   getName.addEventListener("keyup", (e) => {
+    e.preventDefault();
     if (e.keyCode === 13) {
+      if (getName.value === "") return;
       hsName = getName.value;
       container.style.display = "flex";
       getName.style.display = "none";
     }
+  });
+
+  // eventlistener to go back to main menu
+  mmButton.addEventListener("click", () => {
+    mainContainer.style.display = "flex";
+    mainButtons.style.display = "flex";
+    winText.style.display = "none";
+    mmButton.style.display = "none";
+    resetBoard();
+    container.innerHTML = "";
+    createBoard();
+    matches = 0;
   });
 });
